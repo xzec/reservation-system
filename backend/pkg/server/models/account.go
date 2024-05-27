9bb1f53927f101595f7ab6ff45939da191e08b99
Juraj Zec <juro.zec.1@gmail.com>
2024-05-27 22:34:53 +0200

    adjusted Account structure

--- backend/pkg/server/models/account.go
+++ backend/pkg/server/models/account.go
@@ -5,19 +5,18 @@ import (
 )
 
 type Account struct {
-	Id                string    `json:"id"`
-	CreatedAt         time.Time `json:"createdAt"`
-	UpdatedAt         time.Time `json:"updatedAt"`
-	UserId            string    `json:"userId"`
-	Image             string    `json:"image"`
-	Type              string    `json:"type"`
-	Provider          string    `json:"provider"`
-	ProviderAccountId string    `json:"providerAccountId"`
-	RefreshToken      string    `json:"refreshToken"`
-	AccessToken       string    `json:"accessToken"`
-	ExpiresAt         int64     `json:"expiresAt"`
-	IdToken           string    `json:"idToken"`
-	Scope             string    `json:"scope"`
-	SessionState      string    `json:"sessionState"`
-	TokenType         string    `json:"tokenType"`
+	Id                *string    `json:"id"`
+	CreatedAt         *time.Time `json:"-"`
+	UpdatedAt         *time.Time `json:"-"`
+	UserId            *string    `json:"userId"`
+	Type              *string    `json:"type"`
+	Provider          *string    `json:"provider"`
+	ProviderAccountId *string    `json:"providerAccountId"`
+	RefreshToken      *string    `json:"refreshToken"`
+	AccessToken       *string    `json:"accessToken"`
+	ExpiresAt         *int64     `json:"expiresAt"`
+	IdToken           *string    `json:"idToken"`
+	Scope             *string    `json:"scope"`
+	SessionState      *string    `json:"sessionState"`
+	TokenType         *string    `json:"tokenType"`
 }
