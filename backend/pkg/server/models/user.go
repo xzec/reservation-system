e92fc472d020344421ab1ff79bc95e200f1f6f1d
Juraj Zec <juro.zec.1@gmail.com>
2024-05-27 17:26:35 +0200

    added model "User"

--- backend/pkg/server/models/user.go
+++ backend/pkg/server/models/user.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type User struct {
-	Id            string    `json:"id"`
-	CreatedAt     time.Time `json:"createdAt"`
-	UpdatedAt     time.Time `json:"updatedAt"`
-	Email         string    `json:"email"`
-	EmailVerified time.Time `json:"emailVerified"`
-	Name          string    `json:"name"`
-	Image         string    `json:"image"`
+	Id            *string    `json:"id"`
+	CreatedAt     *time.Time `json:"-"`
+	UpdatedAt     *time.Time `json:"-"`
+	Email         *string    `json:"email"`
+	EmailVerified *time.Time `json:"emailVerified"`
+	Name          *string    `json:"name"`
+	Image         *string    `json:"image"`
 }
