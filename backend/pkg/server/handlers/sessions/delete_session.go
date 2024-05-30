ce14c3a50c5aa6ebf53cd7094db2e7d37f821d6c
Juraj Zec <juro.zec.1@gmail.com>
2024-05-30 23:02:24 +0200

    simplified deleteSession handler

--- backend/pkg/server/handlers/sessions/delete_session.go
+++ backend/pkg/server/handlers/sessions/delete_session.go
@@ -13,43 +13,20 @@ import (
 func DeleteSessionHandler(pool *pgxpool.Pool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		sessionToken := r.PathValue("sessionToken")
-		ctx := context.Background()
 
-		tx, err := pool.Begin(ctx)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		defer tx.Rollback(ctx)
-
-		sql1 := "select id, session_token, user_id, expires from sessions where session_token=$1"
+		sql := `
+delete from sessions
+where session_token=$1
+returning id, session_token, user_id, expires`
 
 		var session models.Session
-		err = tx.QueryRow(ctx, sql1, sessionToken).Scan(&session.Id, &session.SessionToken, &session.UserId, &session.Expires)
+		err := pool.QueryRow(context.Background(), sql, sessionToken).Scan(&session.Id, &session.SessionToken, &session.UserId, &session.Expires)
 		if errors.Is(err, pgx.ErrNoRows) {
 			w.WriteHeader(http.StatusNotFound)
 			_, err = w.Write([]byte("null"))
 			return
 		}
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		sql2 := "delete from sessions where session_token=$1"
-
-		commandTag, err := tx.Exec(ctx, sql2, sessionToken)
-		if err != nil {
-			http.Error(w, "Failed to delete the session.", http.StatusInternalServerError)
-			return
-		}
-		if commandTag.RowsAffected() == 0 {
-			http.Error(w, "Session was not found when deleting.", http.StatusNotFound)
-			return
-		}
-
-		if err = tx.Commit(ctx); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
